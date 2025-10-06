3f2dbaef0e797369af74b3a1ae398028204300f8
Matthew Humphreys <mvdh7@users.noreply.github.com>
2025-10-06 15:34:39 +0200

    Remove unused plotly code

--- plot_nuts_dic.py
+++ plot_nuts_dic.py
@@ -1,5 +1,4 @@
 # %%
-import plotly.graph_objects as go
 from matplotlib import pyplot as plt
 
 from read_datasets import dbs
@@ -42,24 +41,3 @@ ax.set_ylabel("DIC / µmol kg$^{-1}$")
 ax.legend(loc="lower left")
 fig.tight_layout()
 fig.savefig("figures/plot_nuts_dic.png")
-
-# %%
-L = dbs.nuts
-marker_color = []
-for i, row in dbs[L].iterrows():
-    if False:
-        pass
-    elif not row.nuts_good:
-        marker_color.append("black")
-    elif row.dic_cell_id == "C_Feb03-22_0802":
-        marker_color.append("red")
-    else:
-        marker_color.append("blue")
-scatter = go.Scatter(
-    x=dbs[L].datetime_analysis,
-    y=dbs[L].dic,
-    mode="markers",
-    marker_color=marker_color,
-)
-fig = go.Figure(scatter)
-fig.show()
